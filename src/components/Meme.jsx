03f125b78e72b2d409b5a72b2d009e5afe8f351c
Yadav106 <sahil.jp.yadav@gmail.com>
2023-01-12 23:38:37 +0530

    implemented the function for text insertion

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -12,28 +12,37 @@ export default function Meme() {
 
     const [allMemeImages, setALlMemeImages] = React.useState(memesData)
 
-    // const [url, setUrl] = React.useState('')
-
-    function handleOnClick() {
+    function handleOnClick(event) {
         let memesDataArray = allMemeImages.data.memes
         let x = Math.floor(Math.random()*memesDataArray.length)
-        // setUrl(memesDataArray[x].url);
-        // console.log(memesDataArray[x].url);
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: memesDataArray[x].url
         }))
     }
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
     return(
         <div className="main">
             <div className="meme">
                 <div className="meme--inputs">
-                    <input type="text" className="input--text" placeholder="Top Text"/>
-                    <input type="text" className="input--text" placeholder="Bottom Text"/>
+                    <input type="text" className="input--text" placeholder="Top Text" name="topText" onChange={handleChange}/>
+                    <input type="text" className="input--text" placeholder="Bottom Text" name="bottomText" onChange={handleChange}/>
                 </div>
                 <button className="meme--button" onClick={handleOnClick}>GET A NEW MEME IMAGE 🖼️</button>
             </div>
-            {meme.randomImage && <img src={meme.randomImage} className="meme--image"/>}
+            <div className="meme--image">
+                {meme.randomImage && <img src={meme.randomImage} className="memeContainer"/>}
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </div>
     )
 }
